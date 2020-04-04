4c5f879d279adca842ee3b5d23f421431b734db6
SitamMatt <mateuszszewc@op.pl>
2020-04-04 12:55:23 +0200

    Release cleanup

--- Client/ACPage.xaml.cs
+++ Client/ACPage.xaml.cs
@@ -1,18 +1,7 @@
-﻿using Cwiczenie4;
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 using SFML.Audio;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Client
 {
